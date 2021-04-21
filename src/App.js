9138a5cab04c68915016fd0d11446d6b530baaf1
linchengyang <18960807585@163.com>
2021-04-21 11:09:05 +0800

    Modify the top icon

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import {Row, Col} from 'antd';
 import 'antd/dist/antd.css';
 import Dashboard from "./pages/dashboard";
 import logoImg from './image/logo.png'
-import github from './image/github.png'
-import twitter from './image/twitter.png'
-import fly from './image/fly.png'
-import pig from './image/pig.png'
+import github from './image/foot/footGithub.png'
+import twitter from './image/foot/footTwitter.png'
+import fly from './image/foot/footFly.png'
 import 'aos/dist/aos.css';
 import AOS from 'aos';
 
@@ -23,21 +22,19 @@ function App() {
           <Col span={2}/>
           <Col span={4}><a href={""}><img className="logoImg" src={logoImg}/></a></Col>
           <Col span={12}/>
-          <Col span={1}><a href={"#"}><img src={github}/></a></Col>
-          <Col span={1}><a href={"#"}><img src={twitter}/></a></Col>
-          <Col span={1}><a href={"#"}><img src={fly}/></a></Col>
-          <Col span={1}><a href={"#"}><img src={pig}/></a></Col>
+          <Col span={1}><a href={"#"}><img className="github" src={github}/></a></Col>
+          <Col span={1}><a href={"#"}><img className="twitter" src={twitter}/></a></Col>
+          <Col span={1}><a href={"#"}><img className="fly" src={fly}/></a></Col>
         </Row>
       </div>
       <div className="menuMobile">
         <Row>
           <Col span={2}/>
           <Col span={4}><img className="logoImg" src={logoImg}/></Col>
-          <Col span={4}/>
-          <Col span={3}><img src={github}/></Col>
-          <Col span={3}><img src={twitter}/></Col>
-          <Col span={3}><img src={fly}/></Col>
-          <Col span={3}><img src={pig}/></Col>
+          <Col span={8}/>
+          <Col span={3}><img className="github" src={github}/></Col>
+          <Col span={3}><img className="github" src={twitter}/></Col>
+          <Col span={3}><img className="fly" src={fly}/></Col>
         </Row>
       </div>
       <Dashboard/>
